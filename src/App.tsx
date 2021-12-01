b014ed6148cfb33d32a3db32eb93b373d167d214
alanbruno1994 <bruno47rios@outlook.com>
2021-11-30 21:05:51 -0300

    feat: created a small project using localStorage and useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useReducer } from 'react';
 import './App.css';
 
+type Action={
+  type:string
+  payload?:any  
+}
+
+const initialValues={
+  email:'',
+  password:'',
+  logged:false
+}
+//Essa funcao e usada para evoluir o estado
+//state e o estado atual
+function reducer(state:any,action:Action)
+{
+    switch(action.type)//type indica o que sera evoluido
+    {
+      case 'email':
+        return  {...state,email:action.payload}
+      case 'password':
+        return  {...state,password:action.payload}
+      case 'logged':
+          return  {...state,logged:action.payload}  
+      default:
+         return {...state}
+    }
+}
+
+
+
 function App() {
+  //o dispatch vai ser uma funcao que no fundo chamada reducer
+  //no dispatch voce so passa o valor do action
+  const [state,dispatch]= useReducer(reducer,initialValues)
+
+  useEffect(()=>{//Aqui esta usando o localStorage
+    const logged=localStorage.getItem('Logged');
+    if(logged==='1'){
+      dispatch({...state,type:'logged',payload:true})
+    }
+  },[])
+
+  function onSubmit(event:any){
+    event.preventDefault()
+    if(state.email==='root' && state.password==='12345')
+    {
+      localStorage.setItem('Logged','1');
+      dispatch({...state,type:'logged',payload:true})
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form onSubmit={onSubmit}>
+          <input value={state.email} onChange={event=>dispatch({type:'email',payload:event.target.value})} />
+          <input type="password" value={state.password} onChange={event=>dispatch({type:'password',payload:event.target.value})} />
+          <input type='submit' value='Click'></input>
+          <div>{state.logged ? 'Entering System':'No Entering'}</div>
+         </form>
     </div>
   );
 }
